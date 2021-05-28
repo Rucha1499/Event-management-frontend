3a63ff8e1ef329290e9990f9aa95449160336ba3
rucha.patil18@vit.edu <rucha.patil18@vit.edu>
2021-05-28 16:51:49 +0530

    adding files for cards in project

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div> Hello Rucha</div>
+    <div className={styles.title}> Hello Rucha</div>
   )
 }
