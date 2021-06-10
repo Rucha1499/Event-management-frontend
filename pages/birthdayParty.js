10dbd14fcf6e7cb903e0b3fa65759a3a2d7947da
rucha.patil18@vit.edu <rucha.patil18@vit.edu>
2021-06-10 11:34:40 +0530

    refactoring code, making API calls for data

--- pages/birthdayParty.js
+++ pages/birthdayParty.js
@@ -1,23 +1,52 @@
 import Head from 'next/head';
+import { useState, useEffect } from 'react';
 import ProductCardsHolder from '../components/ProductCardsHolder/index';
 import Navbar from '../components/Navbar/index';
-import cakeData from '../data/birthdayPartyData/cake';
-import decorationData from '../data/birthdayPartyData/decoration';
-import returnGiftData from '../data/birthdayPartyData/returnGift';
 import styles from '../styles/Home.module.css';
+import BASE_URL from '../constants';
 
-const BirthdayPartyPage = () => (
-  <div className={styles.mainContainerContent}>
-    <Head>
-      <title>
-        Birthday
-      </title>
-    </Head>
-    <Navbar appName="Event Planner" page="Birthday" />
-    <ProductCardsHolder title="Birthday Cakes" data={cakeData} />
-    <ProductCardsHolder title="Birthday Decoration" data={decorationData} />
-    <ProductCardsHolder title="Return Gift" data={returnGiftData} />
-  </div>
-);
+const BirthdayPartyPage = () => {
+  const [cakeData, setCakeData] = useState();
+  const [decorationData, setDecorationData] = useState();
+  const [returnGiftData, setReturnGiftData] = useState();
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/cakeData`)
+      .then((res) => res.json())
+      .then((data) => {
+        setCakeData(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/decorationData`)
+      .then((res) => res.json())
+      .then((data) => {
+        setDecorationData(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/returnGiftData`)
+      .then((res) => res.json())
+      .then((data) => {
+        setReturnGiftData(data);
+      });
+  }, []);
+
+  return (
+    <div className={styles.mainContainerContent}>
+      <Head>
+        <title>
+          Birthday
+        </title>
+      </Head>
+      <Navbar appName="Event Planner" page="Birthday" />
+      {cakeData ? <ProductCardsHolder title="Birthday Cakes" data={cakeData} /> : 'Loading...'}
+      {decorationData && <ProductCardsHolder title="Birthday Decoration" data={decorationData} />}
+      {returnGiftData && <ProductCardsHolder title="Return Gift" data={returnGiftData} />}
+    </div>
+  );
+};
 
 export default BirthdayPartyPage;
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,45 @@
 import Head from 'next/head';
+import { useState, useEffect } from 'react';
 import EventCard from '../components/EventCard/index';
 import Navbar from '../components/Navbar/index';
 import styles from '../styles/Home.module.css';
-import Data from '../data/eventData';
 
-const HomePage = () => (
-  <div className={styles.mainContainer}>
-    <Head>
-      <title>Home page</title>
-    </Head>
-    <Navbar appName="Event Planner" page="" />
-    <div className={styles.mainTitle}>
-      Welcome to Event Planner!📝
+const HomePage = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch('https://event-planner-json-server.herokuapp.com/eventData')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
+  return (
+    <div className={styles.mainContainer}>
+      <Head>
+        <title>Home page</title>
+      </Head>
+      <Navbar appName="Event Planner" page="" />
+      <div className={styles.mainTitle}>
+        Welcome to Event Planner!📝
+      </div>
+      <div className={styles.description}>
+        We help you organize awesome events! Find everything you want for your event to rock.
+        Start Exploring!
+      </div>
+      <div className={styles.cardContainer}>
+        {data ? data.map((eventType) => (
+          <EventCard
+            key={eventType.id}
+            eventName={eventType.eventName}
+            description={eventType.description}
+            link={eventType.link}
+          />
+        )) : 'Loading...'}
+      </div>
     </div>
-    <div className={styles.description}>
-      We help you organize awesome events! Explore what we have for you...
-    </div>
-    <div className={styles.cardContainer}>
-      {Data.map((eventType) => (
-        <EventCard
-          key={eventType.id}
-          eventName={eventType.eventName}
-          description={eventType.description}
-          link={eventType.link}
-        />
-      ))}
-    </div>
-  </div>
-);
+  );
+};
 
 export default HomePage;
